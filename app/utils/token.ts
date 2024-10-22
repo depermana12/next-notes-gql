0c965393177b825b14b7a95706707d8fb4a6c236
depermana12 <deddiapermana97@gmail.com>
2024-10-22 14:45:58 +0700

    add some utils

---
+++ app/utils/token.ts
@@ -0,0 +1,17 @@
+export const tokenKey = "my-token";
+
+export const getToken = (): string | null => {
+  if (typeof window !== "undefined") {
+    return localStorage.getItem(tokenKey);
+  }
+  return null;
+};
+
+export const setToken = (token: string): void => {
+  if (typeof window !== "undefined")
+    return localStorage.setItem(tokenKey, token);
+};
+
+export const isAuth = (): boolean => {
+  return Boolean(getToken());
+};
